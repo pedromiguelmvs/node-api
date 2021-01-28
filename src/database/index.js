66d8db5df7cbd64ec95dda7ebbf55028a18946d1
pedromiguelmvs <pedromiguelmvs@gmail.com>
2021-01-28 19:03:32 -0300

    alteração nos dados da conexão com a DB

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://taky:taky@omnistack10.o7zmp.mongodb.net/api?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://<myUsername>:<myPasswd>@omnistack10.o7zmp.mongodb.net/api?retryWrites=true&w=majority',
 { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
